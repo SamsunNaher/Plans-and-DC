14a5aaadd97bbdb8c50f44cec1cf414ed9b351fd
SamsunNaher <naher.iu.10@gmail.com>
2020-10-12 15:02:06 -0600

    Just started

---
+++ Plans.R
@@ -0,0 +1,3 @@
+setwd("D:/OneDrive - University of New Mexico/Paper/Chapter 3/Data/CPS")
+dir()
+cps_00002<- read.csv("D:/OneDrive - University of New Mexico/Paper/Chapter 3/Data/CPS/cps_00002.csv")
